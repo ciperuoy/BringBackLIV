4a9db6528fd2f01742b0ded72409c60cb9e5e057
defaultuser0 <axolotlslmao@gmail.com>
2024-12-13 19:44:27 -0600

    yuh

---
+++ Plugin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using BepInEx;
+using sigmamodtemplate;
+using UnityEngine;
+
+namespace sigmamodtemplate
+{
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+        private readonly string[] targetNames =
+{
+            "LCKWallCameraSpawner"
+        };
+
+        private void DisableTargetObjects()
+        {
+            foreach (GameObject obj in Resources.FindObjectsOfTypeAll<GameObject>())
+            {
+                if (obj.activeInHierarchy && MatchesTargetName(obj.name))
+                {
+                    obj.SetActive(false);
+                    Debug.Log($"Disabled object: {obj.name}");
+                }
+            }
+        }
+
+        private bool MatchesTargetName(string name)
+        {
+            name = name.ToLower();
+            foreach (string targetName in targetNames)
+            {
+                if (name == targetName.ToLower()) // Match exact names
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        void Start()
+		{
+            HarmonyPatches.ApplyHarmonyPatches();
+			GorillaTagger.OnPlayerSpawned(Initialized);
+		}
+
+		void Initialized()
+		{
+            DisableTargetObjects();
+        }
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace sigmamodtemplate
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "defaultuser0.nomoreliv";
+		public const string Name = "NoMoreLIV";
+		public const string Version = "1.0.0";
+	}
+}
